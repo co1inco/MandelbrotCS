7fb1b401c9ce63bec22dc94372a7411d1f99cd9e
co1inco <colin.meihoefer@gmx.de>
2019-08-28 00:49:36 +0200

    Added Juliaset

--- mandelbrot/Form1.Designer.cs
+++ mandelbrot/Form1.Designer.cs
@@ -73,6 +73,8 @@
             this.pictureBox1.Size = new System.Drawing.Size(750, 751);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             // 
             // numericUpDown_yOffset
             // 
--- mandelbrot/Form1.cs
+++ mandelbrot/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace mandelbrot
 {
@@ -19,6 +20,7 @@ namespace mandelbrot
             InitializeComponent();
 
             CalculateMandelbrot();
+ //           CalculateJulia(new ImgNumber());
 
         }
 
@@ -40,7 +42,8 @@ namespace mandelbrot
                 {
                     double re = map(i, 0, canvase.Width, xMin, xMax);
                     double img = map(j, 0, canvase.Height, yMin, yMax);
-
+                    
+                    /*
                     if ((re < 1.01 && re > 0.99) && ((re < 0.01 && re > -0.01) || (img < 0.01 && img > -0.01)) ||
                         (re > -1.01 && re < -0.99) && ((re < 0.01 && re > -0.01) || (img < 0.01 && img > -0.01)) ||
                         (img > -1.01 && img < -0.99) && ((re < 0.01 && re > -0.01) || (img < 0.01 && img > -0.01)) ||
@@ -54,10 +57,14 @@ namespace mandelbrot
                         canvase.SetPixel(i, j, Color.Red);
                         continue;
                     }
+                    */
 
                     ImgNumber c = new ImgNumber(re, img);
 
                     int it = GetExit(new ImgNumber(), c, maxIterations);
+                    //int it = GetExit(c, new ImgNumber(0, -1), maxIterations);
+
+
 
                     Color pixelColor = new Color();
 
@@ -77,6 +84,49 @@ namespace mandelbrot
             this.pictureBox1.Image = canvase;
         }
 
+        public void CalculateJulia(ImgNumber z)
+        {
+
+            double zoom = (double)numericUpDown_zoom.Value;
+
+            double xMin = 0 - zoom + (double)numericUpDown_xOffset.Value;
+            double xMax = 0 + zoom + (double)numericUpDown_xOffset.Value;
+
+            double yMin = 0 - zoom - (double)numericUpDown_yOffset.Value;
+            double yMax = 0 + zoom - (double)numericUpDown_yOffset.Value;
+
+            int maxIterations = (int)numericUpDown_Iterations.Value;
+
+            for (int i = 0; i < canvase.Width; i++)
+            {
+                for (int j = 0; j < canvase.Height; j++)
+                {
+                    double re = map(i, 0, canvase.Width, xMin, xMax);
+                    double img = map(j, 0, canvase.Height, yMin, yMax);
+
+                    ImgNumber c = new ImgNumber(re, img);
+
+                    //int it = GetExit(new ImgNumber(), c, maxIterations);
+                    int it = GetExit(c, z, maxIterations);
+
+                    Color pixelColor = new Color();
+
+                    if (it == -1)
+                        pixelColor = Color.Black;
+                    else if (it == 1)
+                        pixelColor = Color.Gray;
+                    else
+                    {
+                        int colorGrad = (int)map(it, 0, maxIterations, 30, 255);
+                        pixelColor = Color.FromArgb(colorGrad, colorGrad, colorGrad);
+                    }
+
+                    canvase.SetPixel(i, j, pixelColor);
+                }
+            }
+            this.pictureBox1.Image = canvase;
+            
+        }
 
         private int GetExit(ImgNumber start, ImgNumber c, int iterations)
         {
@@ -116,6 +166,22 @@ namespace mandelbrot
         private void numericUpDown_yOffset_ValueChanged(object sender, EventArgs e)
         {
             CalculateMandelbrot();
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            var pos = pictureBox1.PointToClient(Cursor.Position);
+            if (Control.MouseButtons == MouseButtons.Left && false)
+            {
+                double re = map(pos.X, 0, canvase.Width, -2, 2);
+                double img = map(pos.Y, 0, canvase.Height, -2, 2);
+
+                CalculateJulia(new ImgNumber(re, img));
+            }
         }
     }
 
